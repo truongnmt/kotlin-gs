dbec70d8f0c0e509c302a367b1e1e0f39fee0211
truongnmt <truongnmt.dev@gmail.com>
2022-09-01 16:18:58 +0900

    Data classes

--- src/main/kotlin/Accessmodifiers.kt
+++ src/main/kotlin/Accessmodifiers.kt
@@ -8,7 +8,16 @@ fun main() {
     emp2.fullTime = false // call set()
     println(emp2.firstName)
     println(emp2.fullTime) // call get()
+
+    val car = Car("blue", "Toyota", 2015)
+    println(car)
+    val car2 = car.copy()
+    println(car2)
+    val car4 = car.copy(year = 2016, color = "green")
+    println(car4)
 }
+
+data class Car(val color: String, val model: String, val year: Int)
 
 class Employee (val firstName: String, fullTime: Boolean = true) {
 
