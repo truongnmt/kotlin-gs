7ccd5b62f5db9b286d7e943814ae1221eb95ba9b
truongnmt <truongnmt.dev@gmail.com>
2022-09-05 19:02:38 +0900

    Constructor
    Constructor delegate to primary constructor by default, secondary constructor must call to parent constructor by super

--- src/main/inheritance/Printer.kt
+++ src/main/inheritance/Printer.kt
@@ -1,7 +1,7 @@
 package main.inheritance
 
 fun main() {
-    val laserPrinter = LaserPrinter("Brother 1234")
+    val laserPrinter = LaserPrinter("Brother 1234", 15)
     laserPrinter.printModel()
 }
 
@@ -13,22 +13,32 @@ abstract class Printer (val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-open class LaserPrinter(modelName: String): Printer(modelName) {
+open class LaserPrinter(modelName: String, ppm: Int): Printer(modelName) {
     // use primary constructor or use constructor
     // constructor(): super()
     final override fun printModel() = println("The model name of this laser printer is $modelName")
     // override fun is open => subclass can continue override this function
     // can use final like L19 if don't want this behavior
     override fun bestSellingPrice(): Double = 129.99
-
-
 }
 
-class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName) {
+class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName, 1000) {
 
     // override fun printModel() = println("this is my way of doing it")
 }
 
+// every constructor has to delegate to primary constructor (if has primary constructor)
+// if not primary constructor, secondary constructor must call to parent constructor by super
+open class Something {
+    val someProp: String
 
+    constructor(someParam: String) {
+        someProp = someParam
+    }
+}
+
+class SomethingElse: Something {
+    constructor(someOtherParam: String): super(someOtherParam)
+}
 
 
