571fa47d103a953eebc93b91b6fa8845ca0f2138
truongnmt <truongnmt.dev@gmail.com>
2022-09-05 19:17:23 +0900

    interface

--- src/main/inheritance/Printer.kt
+++ src/main/inheritance/Printer.kt
@@ -13,7 +13,9 @@ abstract class Printer (val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-open class LaserPrinter(modelName: String, ppm: Int): Printer(modelName) {
+// Class can inheritance another class and interface
+open class LaserPrinter(modelName: String, ppm: Int):
+    Printer(modelName) {
     // use primary constructor or use constructor
     // constructor(): super()
     final override fun printModel() = println("The model name of this laser printer is $modelName")
@@ -29,11 +31,20 @@ class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName, 1000) {
 
 // every constructor has to delegate to primary constructor (if has primary constructor)
 // if not primary constructor, secondary constructor must call to parent constructor by super
-open class Something {
+open class Something: MySubInterface {
     val someProp: String
+    override val number: Int = 25
 
     constructor(someParam: String) {
         someProp = someParam
+    }
+
+    override fun MyFunc(str: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun MySubFun(num: Int): String {
+        TODO("Not yet implemented")
     }
 }
 
@@ -45,5 +56,29 @@ class SomethingElse: Something {
 data class DataClass(val number: Int) {
 
 }
+
+// can have property, no need to be abstract, can provide getter and setter, sub interface can override these getter setter
+interface MyInterface {
+    val number: Int
+
+    // not allow concrete property in interface
+    //val number2: Int = 10
+    // use custom get => interface return a value unless a super class override it
+    val number2: Int
+        // property in interface cannot has backing field
+        // get() = field + 10
+        get() = number + 10
+
+    fun MyFunc(str: String): String
+}
+
+// interface is extendable by default, no need to explicit declare "open"
+interface MySubInterface: MyInterface {
+    fun MySubFun(num: Int): String
+}
+
+
+
+
 
 
