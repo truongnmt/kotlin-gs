dc077c0b927699fad2cc618764d84e512cafe846
truongnmt <truongnmt.dev@gmail.com>
2022-09-05 18:50:12 +0900

    use final to stop override fun to be override in subclass

--- src/main/inheritance/Printer.kt
+++ src/main/inheritance/Printer.kt
@@ -13,9 +13,22 @@ abstract class Printer (val modelName: String) {
     abstract fun bestSellingPrice(): Double
 }
 
-class LaserPrinter(modelName: String): Printer(modelName) {
+open class LaserPrinter(modelName: String): Printer(modelName) {
     // use primary constructor or use constructor
     // constructor(): super()
-    override fun printModel() = println("The model name of this laser printer is $modelName")
+    final override fun printModel() = println("The model name of this laser printer is $modelName")
+    // override fun is open => subclass can continue override this function
+    // can use final like L19 if don't want this behavior
     override fun bestSellingPrice(): Double = 129.99
+
+
 }
+
+class SpecialLaserPrinter(modelName: String): LaserPrinter(modelName) {
+
+    // override fun printModel() = println("this is my way of doing it")
+}
+
+
+
+
