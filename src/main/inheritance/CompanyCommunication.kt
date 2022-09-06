1be2d3fdfe54f38ba36673f3b3240de509bf4921
truongnmt <truongnmt.dev@gmail.com>
2022-09-06 14:17:07 +0900

    enums

--- src/main/inheritance/CompanyCommunication.kt
+++ src/main/inheritance/CompanyCommunication.kt
@@ -25,6 +25,15 @@ fun main() {
         }
     })
     println(thisIsMutable)
+
+    println(Department.ACCOUNTING.getDeptInfo())
+}
+
+enum class Department(val fullName: String, val numEmployees: Int) {
+    HR("Human Resources", 5), IT("Information Technology", 10),
+    ACCOUNTING("Accounting", 3), SALES("Sales", 20);
+
+    fun getDeptInfo() = "The $fullName department has $numEmployees employees"
 }
 
 // declare the class and it's instance at the same time => object keyword
